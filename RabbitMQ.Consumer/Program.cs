5f8b3f19686c32c916973bbae8c8a760cbc0d434
szRR <sezer.tetik2005@gmail.com>
2023-09-06 19:14:43 +0300

    added exhange type of direct

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -12,24 +12,49 @@ ConnectionFactory factory = new()
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-// Configuring Consumer
-channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
 
-// Creating Queue
-channel.QueueDeclare(queue: "first-queue", exclusive: false, durable: true);
+string queueName = channel.QueueDeclare().QueueName;
 
-// Read Message(s) from Queue
+channel.QueueBind(
+    queue: queueName,
+    exchange: "direct-exchange-example",
+    routingKey: "direct-queue-example");
+
 EventingBasicConsumer consumer = new(channel);
-channel.BasicConsume(queue: "first-queue", autoAck: false, consumer: consumer);
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
 
 consumer.Received += (sender, e) =>
 {
-    // In this block, we are processing the message coming from the Producer
-    // e.Body.Span or e.Body.ToArray(): These methods are provides the byte data from queue to us.
-    
-    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
-
-    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false); // Message Acknowledgement
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
 };
+
+#region Advanced Queue
+//// Configuring Consumer
+//channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+//// Creating Queue
+//channel.QueueDeclare(queue: "first-queue", exclusive: false, durable: true);
+
+//// Read Message(s) from Queue
+//EventingBasicConsumer consumer = new(channel);
+//channel.BasicConsume(queue: "first-queue", autoAck: false, consumer: consumer);
+
+//consumer.Received += (sender, e) =>
+//{
+//    // In this block, we are processing the message coming from the Producer
+//    // e.Body.Span or e.Body.ToArray(): These methods are provides the byte data from queue to us.
+
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+//    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false); // Message Acknowledgement
+//};
+
+#endregion
 
 Console.ReadLine();
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -11,22 +11,41 @@ ConnectionFactory factory = new()
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-// Creating Queue
-channel.QueueDeclare(queue: "first-queue", exclusive: false, durable: true);
+// Exchange Declare - Direct
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
 
-// Sending Message to Queue
-
-//byte[] message = Encoding.UTF8.GetBytes("Hello, World!");
-//channel.BasicPublish(exchange: "", routingKey: "first-queue", body: message);
-
-IBasicProperties properties = channel.CreateBasicProperties();
-properties.Persistent = true;
-
-for (int i = 0; i < 1000; i++)
+while (true)
 {
-    await Task.Delay(200);
-    byte[] message = Encoding.UTF8.GetBytes($"Hello, World! {i}");
-    channel.BasicPublish(exchange: "", routingKey: "first-queue", body: message, basicProperties: properties);
+    Console.Write("Mesaj: ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "direct-exchange-example", 
+        routingKey: "direct-queue-example",
+        body: byteMessage);
 }
+
+
+#region Advanced Queue
+//// Creating Queue
+//channel.QueueDeclare(queue: "first-queue", exclusive: false, durable: true);
+
+//// Sending Message to Queue
+
+////byte[] message = Encoding.UTF8.GetBytes("Hello, World!");
+////channel.BasicPublish(exchange: "", routingKey: "first-queue", body: message);
+
+//IBasicProperties properties = channel.CreateBasicProperties();
+//properties.Persistent = true;
+
+//for (int i = 0; i < 1000; i++)
+//{
+//    await Task.Delay(200);
+//    byte[] message = Encoding.UTF8.GetBytes($"Hello, World! {i}");
+//    channel.BasicPublish(exchange: "", routingKey: "first-queue", body: message, basicProperties: properties);
+//}
+
+#endregion
 
 Console.ReadLine();
